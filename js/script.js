f634b573682194b5946579393a2687a8bd61cf93
hasanemam1717 <hasanemam1717@gmail.com>
2024-02-18 16:11:31 +0600

    added a js reuseable js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('hellow world')
+const seatsNumber = document.getElementsByClassName('seatNumber');
+let leftSeat =40;
+let totalSeatSale =0;
+
+for(const seat of seatsNumber){
+    seat.addEventListener('click',function(e){
+        // total sale seat count
+        checkTotalseatSale()
+        function checkTotalseatSale(){
+            if(totalSeatSale <= 3){
+                totalSeatSale = totalSeatSale+1;
+                document.getElementById("countSeat").innerText = totalSeatSale;
+            }else{
+                alert("You can't bye avobe 4 seat.")
+            }
+        }
+        // available seat count
+        availableSeatCoun()
+        function availableSeatCoun(){
+            if(leftSeat !== 0){
+                
+            }
+            else{
+                return alert('seat finished')
+            }
+        }
+        leftSeat=leftSeat-1;
+        document.getElementById('leftSits').innerText = leftSeat;
+    })
+}
